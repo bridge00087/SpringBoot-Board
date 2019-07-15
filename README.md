aa248d85c5397c9523f29fe5388e0ae15885dc95
SONGHYUN KIM <bridge00087@gmail.com>
2019-07-15 19:42:13 +0900

    掲示板の作成

---
+++ README.md
@@ -0,0 +1,2 @@
+## Spring Bootsを利用した掲示板
+このプロジェクトはSpringBootを利用した掲示板になります。
---
+++ src/main/java/com/bridge/board/board/service/BoardService.java
@@ -0,0 +1,18 @@
+package com.bridge.board.board.service;
+
+import java.util.List;
+
+import com.bridge.board.board.dto.BoardDto;
+
+public interface BoardService {
+	
+	List<BoardDto> selectBoardList() throws Exception;
+	
+	void insertBoard(BoardDto board) throws Exception;
+
+	BoardDto selectBoardDetail(int boardIdx) throws Exception;
+
+	void updateBoard(BoardDto board) throws Exception;
+
+	void deleteBoard(int boardIdx) throws Exception;
+}
